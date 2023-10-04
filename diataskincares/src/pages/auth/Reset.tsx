2df9ac1bec2fbe2044a43c03ede8284377dced0b
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-04 19:01:44 +0100

    setting up reset password page

---
+++ diataskincares/src/pages/auth/Reset.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import styles from "./auth.module.scss";
+
+import { Link, useNavigate } from "react-router-dom"
+
+const Reset = () => {
+    return (
+        <section className={styles.auth}>
+            
+        </section>
+    )
+}
