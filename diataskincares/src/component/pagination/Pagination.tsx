62ce81dd5cf2c11126a7b5cca5f08e703979ca8f
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-06 22:23:39 +0100

    contbd

--- diataskincares/src/component/pagination/Pagination.tsx
+++ diataskincares/src/component/pagination/Pagination.tsx
@@ -1,7 +1,23 @@
 import { useState } from "react";
 import styles from "./pagination.module.scss";
 
+interface PaginationProps {
+    currentPage: number;
+    setCurrentPage : (pageNumeber : number ) => void;
+    productsPerPage: number;
+    totalProducts: number;
+}
+const Pagination: React.FC<PaginationProps> = ({
+    currentPage,
+    setCurrentPage,
+    productsPerPage,
+    totalProducts
+}) => {
+    const pageNumeber: number[] = [];
+    const totalPages: number = totalProducts / productsPerPage;
+    const pageNumeberLimit : number = 5;
+    const [ maxPageNumberLimit, setMaxPageNumberLimit ] = useState<number> (5);
+    const [minPageNumberLimit, setMAxPageNumberLimit ] = useState<number> (0);
 
-const Pagination = () => {
     
 }
