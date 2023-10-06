b7a1c76bf21962881b9d4015297b12cb105dee06
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-06 22:49:34 +0100

    the pages navigation

--- diataskincares/src/component/pagination/Pagination.tsx
+++ diataskincares/src/component/pagination/Pagination.tsx
@@ -17,7 +17,32 @@ const Pagination: React.FC<PaginationProps> = ({
     const totalPages: number = totalProducts / productsPerPage;
     const pageNumeberLimit : number = 5;
     const [ maxPageNumberLimit, setMaxPageNumberLimit ] = useState<number> (5);
-    const [minPageNumberLimit, setMAxPageNumberLimit ] = useState<number> (0);
+    const [minPageNumberLimit, setMinPageNumberLimit ] = useState<number> (0);
 
-    
+    const paginate = (pageNumeber : number) => {
+        setCurrentPage(pageNumeber);
+        window.scroll(0, 1500);
+    };
+    const paginateNext = () => {
+        setCurrentPage(currentPage + 1 );
+
+        if ((currentPage + 1) > maxPageNumberLimit) {
+            setMaxPageNumberLimit(maxPageNumberLimit + pageNumeberLimit);
+            setMinPageNumberLimit(minPageNumberLimit + pageNumeberLimit);
+        }
+    };
+    const paginatePrev = () => {
+        setCurrentPage(currentPage - 1);
+
+        if ((currentPage - 1) % pageNumeberLimit === 0) {
+            setMaxPageNumberLimit(maxPageNumberLimit - pageNumeberLimit;)
+            setMinPageNumberLimit(minPageNumberLimit - pageNumeberLimit );
+        }
+    };
+
+    for (let i = 1; 1 <= Math.ceil(totalProducts / productsPerPage); i++) {
+        pageNumeber.push(i);
+    }
+
+    return ()
 }
