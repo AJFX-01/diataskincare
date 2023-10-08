dcba046c22da95e48408b90ec7308ad539297179
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-08 15:54:08 -0700

    neew component

--- diataskincares/src/redux/slice/cartSlice.ts
+++ diataskincares/src/redux/slice/cartSlice.ts
@@ -18,5 +18,22 @@ interface SavedItem extends Product {
 };
 
 interface CartState {
-    
+    cartItems: CartItem[];
+    savedItems: SavedItem[];
+    cartTotalQuantity: number;
+    cartTotalAmounts: number;
+    previousURL: string;
+};
+
+const initialState: CartState = {
+    cartItems:localStorage.getItem("cartItems") ? JSON.parse(localStorage.getItem("cartItems")) : [],
+    savedItems:localStorage.getItem("savedItems") ? JSON.parse(localStorage.getItem("savedItems")) : [],
+    cartTotalQuantity: 0,
+    cartTotalAmounts: 0,
+    previousURL: "",
 }
+
+const cartSlice = createSlice({
+    name: "cart",
+    
+})
