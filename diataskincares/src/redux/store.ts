55c3aa920d4a4d299de16a6d59ce47e492db49a3
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-10 09:53:00 -0700

    done with the store redux

--- diataskincares/src/redux/store.ts
+++ diataskincares/src/redux/store.ts
@@ -1,9 +1,10 @@
-import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
 import productReducer from "./slice/productSlice";
 import filterReducer from "./slice/filterSlice";
 import checkoutReducer from "./slice/checkoutSlice";
 import orderReducer from "./slice/orderSlice";
+import cartReducer from "./slice/cartSlice";
 
 
 const rootReducer = combineReducers({
@@ -14,6 +15,17 @@ const rootReducer = combineReducers({
     checkout: checkoutReducer,
     orders: orderReducer,
 });
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+});
+
+
+export default store;
 
 
 
