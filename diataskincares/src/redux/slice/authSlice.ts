1311c7ab039ac96dc0f2c35ca41aa74301a310ff
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-10 08:42:07 -0700

    authentication slice

---
+++ diataskincares/src/redux/slice/authSlice.ts
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+interface AuthorizationState {
+    isLoggedIn: boolean;
+    email: string |  null;
+    userID: string | null;
+    userName: string | null;
+    users: string[];
+    deletedUsers: string[];
+};
+
+const initialState: AuthorizationState = {
+    isLoggedIn : false,
+    email: null,
+    userID: null,
+    userName: null,
+    users: [],
+    deletedUsers: [] 
+} 
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducers: {
+        SET_ACTIVE_USER: (state, action : { payload : { email : string; userID : string; userName : string }}) => {
+            const { email, userID, userName } = action.payload;
+            state.isLoggedIn = true;
+            state.email = email;
+            state.userName = userName; 
+            state.userID = userID;
+        },
+        REMOVE_ACTIVE_USER: (state) => {
+            state.isLoggedIn = false;
+            state.email = null;
+            state.userName = null;
+            state.userID = null; 
+
+        },
+        DELETE_USERS: (state, action) => {
+            state.deletedUsers = action.payload
+        },
+        STORE_USERS: (state, action) => {
+            state.deletedUsers = action.payload
+        }
+});
+
+export const { SET_ACTIVE_USER, REMOVE_ACTIVE_USER, DELETE_USERS, STORE_USERS } = authSlice.actions;
+
+export const selectIsLoggedIn = (state)
