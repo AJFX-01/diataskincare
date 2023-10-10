3b5b759481acd4f2e1e08f108553f59f7ca96017
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-10 09:45:33 -0700

    storejs

--- diataskincares/src/redux/store.ts
+++ diataskincares/src/redux/store.ts
@@ -1,3 +1,22 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import authReducer from "./slice/authSlice";
-import 
+import productReducer from "./slice/productSlice";
+import filterReducer from "./slice/filterSlice";
+import checkoutReducer from "./slice/checkoutSlice";
+import orderReducer from "./slice/orderSlice";
+
+
+const rootReducer = combineReducers({
+    auth: authReducer,
+    product: productReducer,
+    cart: cartReducer,
+    filter: filterReducer,
+    checkout: checkoutReducer,
+    orders: orderReducer,
+});
+
+
+
+
+
+
