bf6e393d61d06627cf6c898e28a0eb0faee987f5
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-11 16:45:11 -0700

    adding the hooks and card sketon

--- authcontext.tsx
+++ authcontext.tsx
@@ -217,4 +217,43 @@ const ProductDetail: React.FC<ProductDetailProps> = () => {
 };
 
 export default ProductDetail;
- 
+
+
+
+
+
+////
+import React from "react";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import styles from "./productDetails.module.scss";
+
+const CardSkeleton: React.FC = () => {
+  return (
+    <SkeletonTheme baseColor="#e7e7e7" highlightColor="#fff">
+      <div className={styles.details}>
+        <div className={styles.img}>
+          <Skeleton className={styles.img} />
+        </div>
+
+        <div className={`${styles.content} ${styles.all}`}>
+          <Skeleton style={{ height: "3rem", marginBottom: "1.5rem" }} />
+          <Skeleton style={{ height: "1.5rem", marginBottom: "1.5rem" }} />
+          <Skeleton style={{ height: "10rem", marginBottom: "1.5rem" }} />
+          <Skeleton style={{ height: "1.5rem", marginBottom: "1.5rem" }} />
+          <Skeleton style={{ height: "1.5rem", marginBottom: "1.5rem" }} />
+          <Skeleton style={{ height: "1.5rem", marginBottom: "1.5rem" }} />
+          <Skeleton style={{ height: "3rem", marginBottom: "1.5rem" }} />
+          <div style={{ display: "flex" }}>
+            <Skeleton />
+            <Skeleton />
+          </div>
+          <Skeleton style={{ height: "1.5rem", marginBottom: "1.5rem" }} />
+          <Skeleton style={{ height: "1.3rem", marginBottom: "1.5rem" }} />
+        </div>
+      </div>
+    </SkeletonTheme>
+  );
+};
+
+export default CardSkeleton;
---
+++ diataskincares/src/component/product/productDetails/ProductDetail.tsx
@@ -0,0 +1,21 @@
+import React , { useState, useEffect } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import StarRatings from "react-satr-rating";
+
+import { ImEyePlus } from "react-icons/im";
+import { TiCancel } from "react-icons/ti";
+import { MdError } from "react-icons/md";
+import styles from "./productDetails.module.scss";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  ADD_TO_CART,
+  CALCULATE_TOTAL_QUANTITY,
+  REMOVE_FROM_SAVED,
+  SAVE_FOR_LATER,
+  selectCartItems,
+  selectSavedItems,
+} from "../../../redux/slice/cartSlice";
+import useFetchDocuments from "../../../hooks/useFetchDocuments";
+import useFetchCollection from "../../../hooks/useFetchCollection";
+import Card from "../../card/Card";
+import CardSkeleton from "./CardSkeleton";
