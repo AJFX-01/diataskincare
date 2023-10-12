7a850c3beb075b07b5f4dd97cbb34dbf11169812
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-12 13:48:38 -0700

    updating the product deatis

--- diataskincares/src/component/product/productDetails/ProductDetail.tsx
+++ diataskincares/src/component/product/productDetails/ProductDetail.tsx
@@ -1,12 +1,12 @@
 import React , { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import StarRatings from "react-star-ratings";
-
+import StarRatings from "react-star-ratings"
 import { ImEyePlus } from "react-icons/im";
 import { TiCancel } from "react-icons/ti";
 import { MdError } from "react-icons/md";
 import styles from "./productDetails.module.scss";
 import { useDispatch, useSelector } from "react-redux";
+import { BsInfoCircle, BsFillCheckCircleFill } from "react-icons/bs";
 import {
   ADD_TO_CART,
   CALCULATE_TOTAL_QUANTITY,
@@ -27,7 +27,7 @@ interface ProductDetailProps {}
 const ProductDetail: React.FC<ProductDetailProps> = () => {
 
     const { id } = useParams();
-    const  [porduct, setProduct ] = useState<any>(null);
+    const  [product, setProduct ] = useState<any>(null);
     const [ error, setError ] = useState<string>("");
     const dispatch = useDispatch();
     // const { document } = useFetchDocuments("Product", id);
@@ -37,8 +37,47 @@ const ProductDetail: React.FC<ProductDetailProps> = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-        
-    })
+        setProduct(document);
+    }, [document]);
+
+
+    const filteredReviews = data.filter((review: any) => review.productID === id);
+    let newCartQuantity: any[] = [];
+    quantityInCart?.map((qtty: any) => newCartQuantity.push(qtty));
+    
+    const matchCartWithProduct = newCartQuantity.find(
+        (p) => p.name === product?.name
+    );
+
+    const addToCart = (product: any) => {
+        if (product?.Availability === "Out of Stock") {
+            setError(
+                "Sorry, this product is currently out of stock, but you can save for later."
+            );
+            setTimeout(() => setError(""), 10000);
+            return;
+        }
+
+        if (matchCartWithProduct?.cartQuantity >= product.count) {
+            setError(
+                `Sorry, this product currently has a total of ${product.count} items available.` 
+            );
+            window.setTimeout(() => setError(""), 7000);
+            return;
+        }
+
+        dispatch(ADD_TO_CART(product));
+        navigate("/cart");
+        dispatch(CALCULATE_TOTAL_QUANTITY());
+    };
+
+    const addToSaved = (product: any) => {
+        dispatch(SAVE_FOR_LATER(product));
+    };
+
+    const removeFromSaved = (product: any) => {
+        dispatch(REMOVE_FROM_SAVED(product));
+    };
 
     return (
         <section>
@@ -178,3 +217,6 @@ const ProductDetail: React.FC<ProductDetailProps> = () => {
         </section>
       );
 }
+
+
+export default ProductDetail;
