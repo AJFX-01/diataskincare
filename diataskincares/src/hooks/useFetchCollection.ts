b7c7f922fa2a33700c5cf4239caecc09b354a8bc
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-13 15:33:22 +0100

    updating the usefetch collections

--- diataskincares/src/hooks/useFetchCollection.ts
+++ diataskincares/src/hooks/useFetchCollection.ts
@@ -2,4 +2,42 @@ import { collection, onSnapshot, orderBy, query, QueryDocumentSnapshot } from "f
 import { useEffect, useState } from "react";
 import { database } from "../firebase/firebase";
 import { toast } from "react-toastify";
+import { Collection } from "typescript";
+
+interface CollectionData {
+    id: string;
+    [key: string ]: any;
+}
+
+const useFetchCollection = (collection.name : string ) => {
+    const [data, setData] = useState<CollectionData[]>([]);
+    const [loading , setLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        const getCollection = () => {
+            setLoading(true);
+            try {
+                const docRef = collection(database, collectionName);
+                const q = query(docRef, orderBy("createdAt", "desc"));
+                onSnapshot(q, (snapshot) => {
+                    const allData: CollectionData[] = [];
+                    snapshot.docs.forEach((doc: QueryDocumentSnapshot) => {
+                        allData.push({ id: doc.id, ...doc.data()});
+                    });
+                    setData(allData);
+                    setLoading(false);
+                });
+            } catch (error) {
+                setLoading(false);
+                toast.error(error.message);
+            }
+        };
+        getCollection();
+    }, [collection]);
+
+    return { data, loading };
+};
+
+
+export default useFetchCollection;
 
