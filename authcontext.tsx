46dc077d0a30f8e1046a39430aad8064013017b8
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-14 16:54:47 -0700

    updating the network

--- authcontext.tsx
+++ authcontext.tsx
@@ -1,37 +1,27 @@
-import { doc, getDoc, DocumentReference, DocumentSnapshot } from "firebase/firestore";
-import { useEffect, useState } from "react";
-import { toast } from "react-toastify";
-import { database } from "../firebase/firebase";
+import { useState, useEffect } from "react";
 
-interface DocumentData {
-  id: string;
-  // Define other properties based on your document structure
-}
-
-interface FetchDocumentsResult {
-  document: DocumentData | null;
-}
-
-export default function useFetchDocuments(collectionName: string, documentID: string): FetchDocumentsResult {
-  const [document, setDocument] = useState<DocumentData | null>(null);
+const useNetworkStatus = (): boolean => {
+  const [status, setStatus] = useState<boolean>(navigator.onLine);
 
   useEffect(() => {
-    const getDocument = async () => {
-      const docRef: DocumentReference = doc(database, collectionName, documentID);
-      const docSnap: DocumentSnapshot = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        const obj: DocumentData = { id: documentID, ...docSnap.data() as DocumentData };
-        setDocument(obj);
-      } else {
-        toast.error("Document not found", {
-          autoClose: 5000,
-          pauseOnFocusLoss: false,
-        });
-      }
+    const setOnline = () => {
+      setStatus(true);
     };
-    getDocument();
-  }, [collectionName, documentID]);
 
-  return { document };
-}
+    const setOffline = () => {
+      setStatus(false);
+    };
+
+    window.addEventListener("online", setOnline);
+    window.addEventListener("offline", setOffline);
+
+    return () => {
+      window.removeEventListener("online", setOnline);
+      window.removeEventListener("offline", setOffline);
+    };
+  }, []);
+
+  return status;
+};
+
+export default useNetworkStatus;
---
+++ diataskincares/src/hooks/useNetworkStatus.ts
@@ -0,0 +1,24 @@
+import { useState, useEffect } from "react";
+
+
+const useNetworkStatus = (): boolean => { 
+    const [status, setStatus] = useState<boolean>(navigator.onLine);
+
+    useEffect(() => {
+        const setOnline = () => {
+            setStatus(true);
+        };
+        
+        const setOffline = () => {
+            setStatus(false);
+        };
+
+        window.addEventListener("onine", setOnline);
+        window.addEventListener("offine", setOffline);
+
+        return () => {
+            window.removeEventListener("omine", setOnline);
+            window.removeEventListener("offine", setOffline); 
+        };
+    })
+}
