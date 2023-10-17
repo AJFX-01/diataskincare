ad63d65e04bf4135da33620099e4f51526fe235c
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-17 15:00:52 -0700

    updating the auth context

--- authcontext.tsx
+++ authcontext.tsx
@@ -336,23 +336,3 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 
 
 /////
-import { initializeApp } from "firebase/app";
-import { getFirestore, Firestore } from "firebase/firestore";
-import { getAuth, Auth } from "firebase/auth";
-import { getStorage, Storage } from "firebase/storage";
-
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN_URL || "",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID || "",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "",
-};
-
-const app = initializeApp(firebaseConfig);
-const database: Firestore = getFirestore(app);
-const auth: Auth = getAuth(app);
-const storage: Storage = getStorage(app);
-
-export { app, database, auth, storage }
--- diataskincares/src/contexts/auth.ts
+++ diataskincares/src/contexts/auth.ts
@@ -1,4 +1,34 @@
 import React, { useContext, useEffect, useState } from "react";
-import {
-    create
+import { 
+    createUserWithEmailAndPassword,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    onAuthStateChanged,
+    sendPasswordResetEmail,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    signOut,
+    updateEmail,
+    updatePassword,
+    updateProfile,
+    UserCredential,
+    Auth,
+    User,
+} from "firebase/auth";
+
+
+type AuthProviderPRops = {
+    user: User  | null;
+    loading: boolean;
+    userName: string;
+    signup: (email: string, password: string) => Promise<UserCredential>;
+    login: (email: string, password: string) => Promise<UserCredential>;
+    logout: () => Promise<void>;
+    resetPassword: (email:string) => Promise<void>;
+    goggleSignIn: () => Promise<UserCredential>;
+    facebookSiginIn: () => Promise<UserCredential>;
+    updateName: (displayName: string) => Promise<void>;
+    updateMail: (newmail: string) => Promise<void>;
+    updatePass: (password: string) => Promise<void>;
+    setUserName: (name: string) => void;
 }
