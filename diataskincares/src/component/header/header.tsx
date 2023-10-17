d997f3c9d50e75b7d96797234af90f9fdc2d79c3
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-17 14:13:00 -0700

    coding up the authentication contexts

---
+++ diataskincares/src/component/header/header.tsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react"
+import { NavLink, useNavigate,  Link } from "react-router-dom"
+import {
+    RiMenuAddLine,
+    RiShoppingCartLine,
+    VscEyeClosed,
+  } from "react-icons/all";
+  import styles from "./header.module.scss";
+
+
+  
