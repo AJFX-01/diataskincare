ad4952b2b5155a9e243d4cc4f7edc5666450b654
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-19 20:00:24 +0100

    created the on login and logout form

--- diataskincares/src/component/header/header.tsx
+++ diataskincares/src/component/header/header.tsx
@@ -5,7 +5,23 @@ import {
     RiShoppingCartLine,
     VscEyeClosed,
   } from "react-icons/all";
-  import styles from "./header.module.scss";
+import styles from "./header.module.scss";
+import { useAuth } from "../../contexts/auth";
+import { auth } from "../../firebase/firebase";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  SET_ACTIVE_USER,
+  REMOVE_ACTIVE_USER,
+} from "../../redux/slice/authSlice";
+import { onAuthStateChanged } from "firebase/auth";
+import ShowOnLogin from "../hiddenLinks/ShowOnLogin";
+import ShowOnLogout from "../hiddenLinks/ShowOnLogout";
+import AdminOnlyLink from "../adminOnlyRoute/AdminOnlyLink";
+import {
+  CALCULATE_TOTAL_QUANTITY,
+  selectCartTotalQuantity,
+  selectSavedItems,
+} from "../../redux/slice/cartSlice";
 
 
   
