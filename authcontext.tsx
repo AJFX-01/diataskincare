20917bc37e77db27050c319745c09c519f1c84f9
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-21 05:37:58 -0700

    new

--- authcontext.tsx
+++ authcontext.tsx
@@ -205,34 +205,4 @@ export default function Header() {
 }
 /
 
-import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from '../../redux/slice/authSlice';
-import { ReactNode } from 'react'; // Import ReactNode type
 
-interface ShowOnLoginProps {
-  children: ReactNode; // Define children prop as ReactNode
-}
-
-export const ShowOnLogin: React.FC<ShowOnLoginProps> = ({ children }) => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  if (isLoggedIn) {
-    return <>{children}</>; // Use fragment to return children
-  }
-  return null;
-};
-
-interface ShowOnLogoutProps {
-  children: ReactNode; // Define children prop as ReactNode
-}
-
-export const ShowOnLogout: React.FC<ShowOnLogoutProps> = ({ children }) => {
-  const isLoggedIn = useSelector(selectIsLoggedIn);
-
-  if (!isLoggedIn) {
-    return <>{children}</>; // Use fragment to return children
-  }
-  return null;
-};
-
-export default ShowOnLogin;
