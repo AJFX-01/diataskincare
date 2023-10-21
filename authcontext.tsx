14e2de60b7eaa527d8eace1bf8641c2aeccbb32a
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-21 09:10:37 -0700

    workingadmin  the

--- authcontext.tsx
+++ authcontext.tsx
@@ -259,3 +259,57 @@ export function AdminOnlyLink({ children }: AdminOnlyLinkProps) {
   }
 }
 
+////
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { selectEmail } from "../../redux/slice/authSlice";
+import { ReactNode } from "react"; // Import ReactNode type
+
+interface AdminOnlyRouteProps {
+  children: ReactNode; // Define children prop as ReactNode
+}
+
+function AdminOnlyRoute({ children }: AdminOnlyRouteProps) {
+  const userEmail = useSelector(selectEmail);
+
+  if (
+    userEmail === process.env.REACT_APP_ADMIN_EMAIL ||
+    userEmail === process.env.REACT_APP_ADMIN_EMAIL_TWO ||
+    userEmail === process.env.REACT_APP_ADMIN_EMAIL_THREE
+  ) {
+    return <>{children}</>;
+  } else {
+    return (
+      <section style={{ minHeight: "80vh" }}>
+        <div className="container">
+          <h2>PERMISSION DENIED</h2>
+          <p>This page can only be viewed by an admin</p>
+          <br />
+          <Link to="/">
+            <button className="--btn --btn-primary">&larr; Back to Home</button>
+          </Link>
+        </div>
+      </section>
+    );
+  }
+}
+
+interface AdminOnlyLinkProps {
+  children: ReactNode; // Define children prop as ReactNode
+}
+
+function AdminOnlyLink({ children }: AdminOnlyLinkProps) {
+  const userEmail = useSelector(selectEmail);
+
+  if (
+    userEmail === process.env.REACT_APP_ADMIN_EMAIL ||
+    userEmail === process.env.REACT_APP_ADMIN_EMAIL_TWO ||
+    userEmail === process.env.REACT_APP_ADMIN_EMAIL_THREE
+  ) {
+    return <>{children}</>;
+  } else {
+    return null;
+  }
+}
+
+export { AdminOnlyRoute, AdminOnlyLink };
---
+++ diataskincares/src/component/adminOnlyRoute/AdminOnlyLink.tsx
@@ -0,0 +1,39 @@
+import { useSelector } from "react-redux";
+import  { Link } from "react-router-dom";
+import { selectEmail } from "../../redux/slice/authSlice";
+import React, { ReactNode } from "react";
+
+
+interface AdminOnlyRouteProps {
+    children: ReactNode; // Define children prop as ReactNode
+}
+
+function AdminOnlyRoute({ children }: AdminOnlyRouteProps) {
+    const userEmail = useSelector(selectEmail);
+
+    if (
+        userEmail === process.env.REACT_APP_ADMIN_EMAIL || 
+        userEmail === process.env.REACT_APP_ADMIN_EMAIL_TWO ||
+        userEmail === process.env.REACT_APP_ADMIN_EMAI_THREE  
+    ) {
+        return <>{children}</>;
+    } else {
+        return(
+            <section style={{ minHeight: "80vh"}}>
+                <div className="container">
+                    <h2>PERMISSION DENIED</h2>
+                    <p>This page can only be viewed by an admin</p>
+                    <br />
+                    <Link to="/">
+                        <button className="--btn --btn-primary">&larr; Back to Home</button>
+                    </Link>
+                </div>
+            </section>
+        );
+    }
+}
+
+interface AdminOnlyLinkProps {
+    children: ReactNode;
+}
+
