94589888287cf384322e55902a025dc6ebd0c08f
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-21 08:40:07 -0700

    new bug fixes

--- diataskincares/src/component/hiddenLinks/HiddenIiNK.tsx
+++ diataskincares/src/component/hiddenLinks/HiddenIiNK.tsx
@@ -1,7 +1,7 @@
 
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from '../../redux/slice/authSlice';
-import { ReactNode } from 'react'; // Import ReactNode type
+import  React, { ReactNode } from 'react'; // Import ReactNode type
 
 interface ShowOnLoginProps {
   children: ReactNode; // Define children prop as ReactNode
@@ -26,4 +26,4 @@ export const ShowOnLogout: React.FC<ShowOnLogoutProps> = ({ children }) => {
   if (!isLoggedIn) {
     return <>{children}</>; // Use fragment to return children
   }
-  return null;
+  return null
