88e457e58626d438e4b93ea56535ff22d8a2abe4
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-22 10:15:19 -0700

    done with the range

--- diataskincares/src/component/range/range.tsx
+++ diataskincares/src/component/range/range.tsx
@@ -9,6 +9,25 @@ interface RangeData {
 
 const Range = () => {
     return (
-        <section className={`container ${styles.range}`}></section>
-    )
+        <section className={`container ${styles.range}`}>
+            <h2>What types of products do we offer?</h2>
+            <div className={styles["range-grid"]}>
+                {rangeData.map((range: RangeData) => {
+                    const { id, title, image } = range;
+                    return (
+                        <div key={id} className={styles["range-data"]}>
+                            <div className={styles["range-image"]}>
+                                <img src={image} alt="" />
+                            </div>
+                            <div>
+                                <h1 style={{ color: "#fff"}}>{title}</h1>
+                            </div> 
+                        </div>
+                    );
+                })}
+            </div>
+        </section>
+    );
 }
+
+export default Range;
