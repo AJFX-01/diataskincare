f6692c8e44bae0cd1a95f18762b992969c2cea66
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-22 06:29:10 -0700

    coidng up the footer

---
+++ authcontext.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoIosHome, RiContactsBookUploadFill, RiLuggageCartFill, TiShoppingCart, HiSaveAs, IoNotifications, BiLogOut, FaUserMinus, BiLogIn, HiUserAdd } from 'react-icons/all';
+import { useAuth } from '../../contexts/authContext';
+import { useSelector } from 'react-redux';
+import { selectIsLoggedIn } from '../../redux/slice/authSlice';
+import { useNavigate } from 'react-router-dom';
+import styles from './footer.module.scss';
+
+const Footer: React.FC = () => {
+  const date = new Date();
+  const year = date.getFullYear();
+
+  const { user, logout } = useAuth();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+  const navigate = useNavigate();
+
+  const logoutUser = async () => {
+    await logout();
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.footer}>
+      <ul className='container'>
+        <span>Quick links</span>
+        <div className={styles.flex}>
+          <li>
+            <IoIosHome />
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <RiContactsBookUploadFill />
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li>
+            <RiLuggageCartFill />
+            <Link to='/order-history'>Orders</Link>
+          </li>
+          <li>
+            <TiShoppingCart />
+            <Link to='/cart'>Cart</Link>
+          </li>
+          <li>
+            <HiSaveAs />
+            <Link to='/saved-products'>Saved products</Link>
+          </li>
+
+          {user && (
+            <>
+              <li>
+                <IoNotifications />
+                <Link to='/notifications'>Notifications</Link>
+              </li>
+              <li onClick={logoutUser}>
+                <BiLogOut />
+                <p>Logout</p>
+              </li>
+              <li>
+                <FaUserMinus />
+                <Link to='/delete-account'>Delete Account</Link>
+              </li>
+            </>
+          )}
+          {!isLoggedIn ? (
+            <>
+              <li>
+                <BiLogIn />
+                <Link to='/login'>Login</Link>
+              </li>
+              <li>
+                <HiUserAdd />
+                <Link to='/signup'>Sign up</Link>
+              </li>
+            </>
+          ) : null}
+        </div>
+      </ul>
+
+      <div className={styles.copyright}>&copy; {year} All Rights Reserved</div>
+    </div>
+  );
+};
+
+export default Footer;
