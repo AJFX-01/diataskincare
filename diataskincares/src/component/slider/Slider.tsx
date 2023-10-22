001b7dfb21b63f6dc90de1102c83171a220f50f2
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-22 11:09:23 -0700

    updating the component

--- diataskincares/src/component/slider/Slider.tsx
+++ diataskincares/src/component/slider/Slider.tsx
@@ -16,5 +16,34 @@ const Slider = () => {
     const [ currentSlide, setCurrentSlide ] = useState<number>(0);
     const slideLength : number = sliderData.length;
 
-    
+    const nextSlide = () => {
+        setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
+    }
+
+    const prevSlide = () => {
+        setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1);
+    }
+
+
+    const autoScroll : boolean = true;
+    let slideInterval: NodeJS.Timeout | null;
+    const intervalTime: number = 4000;
+
+    useEffect(() => {
+        setCurrentSlide(0);
+    }, []);
+
+    useEffect(() => {
+        if (autoScroll) {
+            const auto = () => {
+                slideInterval = setInterval(nextSlide, intervalTime);
+            };
+            auto();
+        }
+        return () => {
+            if (slideInterval) {
+                clearInterval(slideInterval);
+            }
+        };
+    }, [currentSlide, slideInterval, autoScroll]);
 } 
