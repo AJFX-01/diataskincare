021479b38ac64e0d7da5e0cc703ad1300deb50e8
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-22 10:22:08 -0700

    range dattas

--- diataskincares/src/component/range/range.tsx
+++ diataskincares/src/component/range/range.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./range.module.scss";
+import { rangeData } from "./rangeDAta";
 
 interface RangeData {
     id: number;
---
+++ diataskincares/src/component/range/rangeDAta.ts
@@ -0,0 +1,29 @@
+export interface RangeItem {
+    id: number;
+    title: string;
+    image: string;
+  }
+  
+  export const rangeData: RangeItem[] = [
+    {
+      id: 1,
+      title: "Fashion",
+      image: "fashion.png",
+    },
+    {
+      id: 2,
+      title: "Appliances",
+      image: "appliances.jpeg",
+    },
+    {
+      id: 3,
+      title: "Gadgets",
+      image: "gadgets.png",
+    },
+    {
+      id: 4,
+      title: "Accessories",
+      image: "accessories.webp",
+    },
+  ];
+  
