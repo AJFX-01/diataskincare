72bd0a4d5819af4cdad98bfe1fc20e88db69efe5
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-22 10:39:30 -0700

    compete the sider data array

---
+++ diataskincares/src/component/slider/sliderData.ts
@@ -0,0 +1,29 @@
+export interface SliderItem {
+    image: string;
+    heading: string;
+    desc: string;
+  }
+  
+  export const sliderData: SliderItem[] = [
+    {
+      image: "https://images.unsplash.com/photo-1526738549149-8e07eca6c147?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fHRlY2glMjBnYWRnZXRzfGVufDB8fDB8fA%3D%3D&w=1000&q=80",
+      heading: "Shoes Villa",
+      desc: "Discount on all onsale products.",
+    },
+    {
+      image: "https://styla-prod-us.imgix.net/f78b447d-5eda-41e8-b14e-a8e2162f0d36/1648500691218_8fd6bd3c-7d27-45f5-9827-964be7d8239c?auto=format%2Ccompress&w=3840&h=1800&fit=crop&crop=faces%2Cedges",
+      heading: "Women Fashion",
+      desc: "Discount on all onsale products.",
+    },
+    {
+      image: "https://media.vanityfair.com/photos/61f190b7d25c05ade4095975/master/w_2560%2Cc_limit/winter-accessories-ecomm-2022-lede.jpg",
+      heading: "Men Fashion",
+      desc: "Discount on all onsale products.",
+    },
+    {
+      image: "https://cdn.repeller.com/wp-content/uploads/2017/09/NYFW-Street-Style-Fall-2017-Man-Repeller-Day-3-34-1272x848.jpg",
+      heading: "Awesome Gadgets",
+      desc: "Discount on all onsale products.",
+    },
+  ];
+  
