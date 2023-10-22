d882ff55d209d71c8d0f3c81338389ae950d26b8
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-22 14:38:05 -0700

    done with the infobox

---
+++ diataskincares/src/component/infobox/Infobox.tsx
@@ -0,0 +1,24 @@
+import React, { ReactNode } from "react";
+import Card from "../card/Card";
+import styles from "./infoBox.module.scss";
+
+interface InfoBoxProps {
+    cardClass: string;
+    title: string;
+    count: number;
+    icon: ReactNode;
+}
+
+function InfoBox({ cardClass, title, count, icon }: InfoBoxProps) {
+    return (
+        <div className={styles["info-box"]}>
+            <Card cardClass={cardClass}>
+                <h4>{title}</h4>
+                <span>
+                    <h3>{count}</h3>
+                    {icon}
+                </span>
+            </Card>
+        </div>
+    );
+}
