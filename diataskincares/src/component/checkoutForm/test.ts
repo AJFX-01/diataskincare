d8d0dff8fc07bd75709101fca95be8ea93b16fa4
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-27 04:45:20 -0700

    WORKING ON THE test.tz

---
+++ diataskincares/src/component/checkoutForm/test.ts
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import {
+  PaymentElement,
+  useStripe,
+  useElements,
+} from "@stripe/react-stripe-js";
+import styles from "./checkoutForm.module.scss";
+import Card from "../card/Card";
+import CheckoutSummary from "../checkoutSummary/CheckoutSummary";
+import spinnerImg from "../../assets/spinner.jpg";
+import { toast } from "react-toastify";
