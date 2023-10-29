1f46f4e55f546b4ee2c8b9340219bc4b0d8337cb
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-29 05:42:57 -0700

    working on test

--- diataskincares/src/component/checkoutForm/test.ts
+++ diataskincares/src/component/checkoutForm/test.ts
@@ -10,3 +10,31 @@ import CheckoutSummary from "../checkoutSummary/CheckoutSummary";
 import spinnerImg from "../../assets/spinner.jpg";
 import { toast } from "react-toastify";
 
+interface CheckoutFormProps {
+
+}
+function CheckForm(props: CheckoutFormProps) {
+  const stripe = useStripe();
+  const element = useElements();
+  const [message, setMessage] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  useEffect(() => {
+    if(!stripe) {
+      return;
+    }
+
+    const clientSecret = new URLSearchParams(window.location.search).get(
+      "payment_intent_client_secret"
+    );
+
+    if (!clientSecret) {
+      return;
+    }
+  }, [stripe]);
+
+  const saveOrder = () => {
+    
+  }
+}
