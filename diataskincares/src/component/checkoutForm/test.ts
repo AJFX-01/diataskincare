fbf825913da305d264eb44e2bf4b5c6dc485eb67
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-29 12:49:40 -0700

    done

--- diataskincares/src/component/checkoutForm/test.ts
+++ diataskincares/src/component/checkoutForm/test.ts
@@ -70,6 +70,18 @@ function CheckForm(props: CheckoutFormProps) {
         }
       }
     });
+
+     // ======== this is stripe being specific, we just handled it above =============
+    // This point will only be reached if there is an immediate error when
+    // confirming the payment. Otherwise, your customer will be redirected to
+    // your `return_url`. For some payment methods like iDEAL, your customer will
+    // be redirected to an intermediate site first to authorize the payment, then
+    // redirected to the `return_url`.
+    // if (error.type === "card_error" || error.type === "validation_error") {
+    //   setMessage(error.message);
+    // } else {
+    //   setMessage('An unexpected error occurred.');
+    // }
     
     setIsLoading(false);
   }
