3f1538780a13edaf8238e06c89a1c516b77092c7
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-10-30 00:27:57 -0700

    first commit of the new week

--- diataskincares/src/component/RedrictSucess.tsx
+++ diataskincares/src/component/RedrictSucess.tsx
@@ -8,6 +8,34 @@ interface RedirectSuccessProps {
 
 const RedirectSuccess: React.FC<RedirectSuccessProps> = (props) => {
 
+    const navigate =useNavigate();
+    const [message, setMessage] = useState<string>(
+        props.message || 
+        "Permission denied, this page's contents will only be rendered after a purchase."
+    );
+    
+    window.setTimeout(() => {
+        setMessage("Redirecting to Home.....");
+    }, 8000);
+
+    useEffect(() => {
+        window.setTimeout(() => {
+            navigate('/');
+        }, 12000);
+    }, [navigate]);
+
+    return (
+        <div 
+            className='container'
+            style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: 'center',
+                minHeight: '80vh'
+            }}>
+                <h2>{message}</h2>
+        </div>
+    );
 }
 
 export default RedirectSuccess;
