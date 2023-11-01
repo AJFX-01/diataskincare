5c135cb5bc878e3b8a4af2b8872cbe1af37eb759
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-01 13:57:59 -0700

    updating the utis fie

--- diataskincares/src/test-utils/test-util.tsx
+++ diataskincares/src/test-utils/test-util.tsx
@@ -1,10 +1,12 @@
 import { render, RenderOptions } from '@testing-library/react';
-import { AuthProvider } from '../contexts/authContext';
+import { AuthProvider } from '../contexts/auth';
 import { RenderResult } from '@testing-library/react';
 
+
 const renderWithContext = (ui: React.ReactElement, options?: RenderOptions): RenderResult =>
-  render(ui, { wrapper: AuthProvider, ...options });
+    render(ui, { wrapper: AuthProvider, ...options });
 
 export * from '@testing-library/react';
+
 
 export { renderWithContext as render };
