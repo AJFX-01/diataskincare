c6da764756c19f13bb4ab7e9d970606b4a0fbdf3
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-02 15:13:11 -0700

    new upgrade

--- diataskincares/src/pages/admin/addproduct/Addproduct.tsx
+++ diataskincares/src/pages/admin/addproduct/Addproduct.tsx
@@ -9,8 +9,21 @@ import {
 import { database, storage } from "../../../firebase/firebase";
 import styles from "./addProduct.module.scss";
 import { toast } from "react-toastify";
-import { addDoc, collection, doc, setDoc, Timestamp } from "firebase/firestore";
+import { addDoc, 
+    collection, 
+    doc, 
+    setDoc,
+    Timestamp
+ } from "firebase/firestore";
 import Loader from "../../../component/loader/loader";
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectProducts } from "../../../redux/slice/productSlice";
+import { FcNeutralDecision } from "react-icons/fc";
+import { BsIntersect } from "react-icons/bs";
+
+
+interface Product {
+    id: string;
+    name: string
+}
