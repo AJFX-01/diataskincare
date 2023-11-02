5e0d36437d586bc8b7e202226d2082ef60559315
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-02 08:48:17 -0700

    working on the import module

--- diataskincares/src/pages/admin/addproduct/Addproduct.tsx
+++ diataskincares/src/pages/admin/addproduct/Addproduct.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Card from "../../../components/card/Card";
+import Card from "../../../component/card/Card";
 import {
   ref,
   uploadBytesResumable,
@@ -10,7 +10,7 @@ import { database, storage } from "../../../firebase/firebase";
 import styles from "./addProduct.module.scss";
 import { toast } from "react-toastify";
 import { addDoc, collection, doc, setDoc, Timestamp } from "firebase/firestore";
-import Loader from "../../../components/loader/Loader";
+import Loader from "../../../component/loader/loader";
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { selectProducts } from "../../../redux/slice/productSlice";
