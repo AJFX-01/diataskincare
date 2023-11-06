6234720eded95e2f9a54c14c8852e9cc6b802555
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-06 11:46:27 -0800

    workling on the retrun statement

--- diataskincares/src/pages/admin/users/Users.tsx
+++ diataskincares/src/pages/admin/users/Users.tsx
@@ -13,6 +13,53 @@ import { toast } from "react-toastify";
 
 const User: React.FC = () => {
 
+    const {data, loading} = useFetchCollection("Users");
+    const dispatch = useDispatch();
+    const users = useSelector(selectUsers);
+
+    useEffect(() => {
+        if(data) {
+            dispatch(STORE_USERS(data));
+        }
+    }, [dispatch, data]);
+
+
+    const deleteUserFromDatabase = async (id: string) => {
+        try {
+            await deleteDoc(doc(database, "Users", id));
+            toast.success("User deleted successfully");
+        } catch (error : any) {
+            toast.error(error.message)
+        }
+    }
+
+    const confirmDelete = (id: string, username: string) => {
+        Notiflix.Confirm.show(
+            "Delete User",
+            `Are you sure you want to delete ${username} from the users list?`,
+            "DELETE",
+            "CANCEL",
+            function okCb() {
+                deleteUserFromDatabase(id);
+            },
+            function cancelCb() {},
+             {
+                width: "320px",
+                borderRadius: "5px",
+                titleColor: "#c07d53",
+                okButtonBackground: "#c07d53",
+                cssAnimationStyle: "zoom"
+            }
+        );
+    };
+
+    if (loading) {
+        return <Loader />;
+    }
+
+    if (users.length === 0) {
+        return <p>You have no users at the moment</p>
+    }
 
     return ();
 } 
