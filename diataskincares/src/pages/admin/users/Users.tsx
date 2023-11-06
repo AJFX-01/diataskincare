72d0e338e92cc65bf6675abb8763d822c7dcb578
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-06 11:24:56 -0800

    setting up the user componnets

--- diataskincares/src/pages/admin/users/Users.tsx
+++ diataskincares/src/pages/admin/users/Users.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { selectUsers, STORE_USERS } from "../../../redux/slice/authSlice";
 import useFetchCollection from "../../../hooks/useFetchCollection";
@@ -9,3 +9,13 @@ import styles from "./users.module.scss";
 import Loader from "../../../component/loader/loader";
 import Notiflix from "notiflix";
 import { toast } from "react-toastify";
+
+
+const User: React.FC = () => {
+
+
+    return ();
+} 
+
+
+export default User;
