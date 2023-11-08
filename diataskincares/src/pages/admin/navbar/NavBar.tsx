8c6e2ac89e350e69f66f347b16195dcebc4e067d
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-08 15:41:29 -0800

    done with the navbar componenets

--- diataskincares/src/pages/admin/navbar/NavBar.tsx
+++ diataskincares/src/pages/admin/navbar/NavBar.tsx
@@ -12,8 +12,38 @@ const Navbar = () => {
             <div className={styles.user}>
                 <FaUserCircle size={40} color="#fff" />
                 <h4 style={{ marginTop: '1rem'}}></h4>
+                <b>(ADMIN)</b>
             </div>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink to="/admin/home" className={activeLink}>
+                            Home
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/admin/all-products" className={activeLink}>
+                            View Products
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/admin/add-product/ADD" className={activeLink}>
+                            Add Products
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/admin/orders" className={activeLink}>
+                            Orders
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink to="/admin/users" className={activeLink}>
+                            Users
+                        </NavLink>
+                    </li>
+                </ul>
+            </nav>
         </div>
-    )
+    );
 }
 
