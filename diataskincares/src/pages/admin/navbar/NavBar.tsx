7a6f17408ecc59bcbfa2121c610f205959bbc798
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-08 15:17:41 -0800

    working on the projeect

---
+++ diataskincares/src/pages/admin/navbar/NavBar.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { FaUserCircle } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import { useAuth } from "../../../contexts/auth";
+import styles from "./navbar.module.scss";
