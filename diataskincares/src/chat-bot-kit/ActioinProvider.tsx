7fe13acd29da69fa44a4bb45ad28001a84170bdf
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-10 04:15:58 -0800

    done with the action proviuder

--- diataskincares/src/chat-bot-kit/ActioinProvider.tsx
+++ diataskincares/src/chat-bot-kit/ActioinProvider.tsx
@@ -81,12 +81,17 @@ const ActionProvider: React.FC<ActionProviderProps> = ({
 
     return(
         <div>
-            {React.Children.map(children, (child) => {
+            {React.Children.map(children, (child : any) => {
                 return React.cloneElement(child, {
                     actions: {
-                        
-                    }
-                })
+                        handleHello,
+                        handleProducts,
+                        handleThanks,
+                        handleGood,
+                        handleOkay,
+                        handleAccount,
+                    },
+                });
             })}
         </div>
     );
