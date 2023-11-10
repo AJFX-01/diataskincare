89e0bce16e2b346458e368683df03a181e1e2609
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-10 00:35:57 -0800

    more details

--- diataskincares/src/chat-bot-kit/ActioinProvider.tsx
+++ diataskincares/src/chat-bot-kit/ActioinProvider.tsx
@@ -5,4 +5,7 @@ interface ActionProviderProps {
     createChatBotMessage: (message : string, option?: Record<string , any>) => any;
     setState: React.Dispatch<React.SetStateAction<any>>;
     children: ReactNode;
-},
+};
+
+
+const ActionProvider: React.FC<ActionProviderProps>
