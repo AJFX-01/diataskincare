28510f0032e78748991f52b1d398741da36fc3c0
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-10 05:42:44 -0800

    working the message parser

--- diataskincares/src/chat-bot-kit/MessageParser.tsx
+++ diataskincares/src/chat-bot-kit/MessageParser.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode} from 'react';
 
 interface MessageParserProps {
     children: ReactNode;
@@ -12,4 +12,29 @@ interface MessageParserProps {
     };
 }
 
-const MessageParser : React.FC<MessageParserProps> = ({ children, action})
+const MessageParser : React.FC<MessageParserProps> = ({ children, actions }) => {
+
+    const parse = (message: string) => {
+        if (message.includes('hello') || message.includes('hi') || message.includes('hey') || message.includes('good')) {
+            actions.handleHello();
+        }
+
+        if (message.includes("how")) {
+            actions.handleGood();
+        }
+
+        if (message.includes('good') || message.includes('fine') || message.includes('great') || message.includes('okay')) {
+            actions.handleOkay();
+        }
+        
+        if (message.includes('product') || message.includes('item') || message.includes('help') || message.includes('assistance') || message.includes('password')) {
+            actions.handleProducts();
+        }
+
+    return(
+        <div>non</div>
+    );
+
+}
+
+export default MessageParser;
