efc5e856d84e1955368a67b665a38fa9ea1bc002
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-15 11:28:25 -0800

    westing 2

--- diataskincares/src/contexts/auth.ts
+++ diataskincares/src/contexts/auth.ts
@@ -175,7 +175,7 @@ export const useAuth = () => {
         throw new Error('useAuth must be used within an AuthProvider');
     }
     return context;
-}; hel
+};
 interface AuthProviderProps {
     children: ReactNode;
 }
