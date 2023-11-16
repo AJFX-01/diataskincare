fe3fac0e7a865035c534e1cbfa4f2d28d74d1559
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-16 01:23:22 -0800

    donr

--- diataskincares/src/pages/auth/Register.tsx
+++ diataskincares/src/pages/auth/Register.tsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import signinImg from "../../assets/register.png";
-import Card from "../../components/card/Card";
+import Card from "../../component/card/Card";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "./auth.module.scss";
-import { useAuth } from "../../contexts/authContext";
+import { useAuth } from "../../contexts/auth";
 import { GoPrimitiveDot } from "react-icons/go";
 import { ImCheckmark } from "react-icons/im";
 import { IoIosEye, IoMdEyeOff } from "react-icons/io";
@@ -15,8 +15,53 @@ import { useSelector } from "react-redux";
 import { selectPreviousURL } from "../../redux/slice/cartSlice";
 import { FaGoogle } from "react-icons/fa";
 
-const Register = () => {
+const Register: React.FC = () => {
 
+
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const passwordRef = useRef();
+    const [view, setView] = useState<boolean>(false);
+    const [caseCondition, setCaseCondition] = useState<boolean>(false);
+    const [numberCondition, setNumberCondition] = useState<boolean>(false);
+    const [charCondition, setCharCondition] = useState<boolean>(false);
+    const [lengthCondition, setLengthCondition] = useState<boolean>(false);
+    const [passwordComplete, setPasswordComplete] = useState(false);
+    const [passFocus , setPassFocus] = useState<boolean>(false);
+    const [userName, setUserName] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+    const navigate = useNavigate();
+
+    const { signup, updateName, googleSignIn, user } = useAuth();
+    const previousURL = useSelector(selectPreviousURL);
+
+    const redirectUser = () => {
+        if(previousURL.includes("cart")) {
+            return navigate("/cart")
+        } else {
+            return("/")
+        }
+    
+    };
+
+    const registerUser = async (e) => {
+        e.preventDefault();
+
+
+        try {
+            setLoading(true)
+            setError("");
+            await signup(email, password);
+            await updateName(userName);
+            setLoading(false);
+            redirectUser();
+        } catch (error : any) {
+
+        }
+    }
+
+    
     return (
         <section className={`${styles.auth}`}>
             <Card>
