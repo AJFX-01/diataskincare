ea90d0f610da09a63736b46a15d4e58411a45ba3
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-22 09:52:35 +0100

    done

--- diataskincares/src/pages/checkout/CheckoutDetails.tsx
+++ diataskincares/src/pages/checkout/CheckoutDetails.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Card from "../../component/card/Card";
 import { CountryDropdown } from "react-country-region-selector";
 import styles from "./checkoutDetails.module.scss";
@@ -38,10 +38,101 @@ const CheckoutDetails : Function = () => {
         e.preventDefault();
         dispatch(SAVE_SHIPPING_ADDRESSS(shippingAddress));
         navigate("/checkout");
-    }
+    };
 
     return(
+        <section>
+            <div className={`container ${styles.checkout}`}>
+                <h2>Checkout Details</h2>
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <Card cardClass={styles.card}>
+                            <h3>Shipping Address</h3>
+                            <label>Recipent Name:</label>
+                            <input 
+                                type="text"
+                                placeholder="Recipient name"
+                                required
+                                name="name"
+                                value={shippingAddress.name}
+                                onChange={(e) => handleShipping(e)}
+                            />
+                            <label>Address line 1:</label>
+                            <input 
+                                type="text"
+                                placeholder="Address line 1"
+                                required
+                                name="line1"
+                                value={shippingAddress.line1}
+                                onChange={(e) => handleShipping(e)}
+                            />
+                            <label> Address line 2</label>
+                            <input 
+                                type="text"
+                                placeholder="Address line 2"
+                                required
+                                name="line2"
+                                value={shippingAddress.line2}
+                                onChange={(e) => handleShipping(e)}
+                            />
+                            <label>City</label>
+                            <input 
+                                type="text" 
+                                placeholder="City"
+                                required
+                                name="city"
+                                value={shippingAddress.city}
+                                onChange={(e) => handleShipping(e)}
+                            />
+                            <label>State</label>
+                            <input 
+                                type="text"
+                                placeholder="state"
+                                required
+                                name="state"
+                                value={shippingAddress.state}
+                                onChange={(e) => handleShipping(e)}
+                            />
+                            <label>Postal Code</label>
+                            <input 
+                                type="number"
+                                placeholder="Postal code"
+                                required
+                                name="postal-code"
+                                value={shippingAddress.postal_code}
+                                onChange={(e) => handleShipping(e)}
+                            />
+                            <label>Select Country</label>
+                            <CountryDropdown
+                                className={styles.select}
+                                valueType="short"
+                                value={shippingAddress.country}
+                                onChange={(val) => 
+                                    handleShipping({
+                                        target: { name: "country", value: val},
+                                    })
+                                }
+                            />
+                            <label>Phone:</label>
+                            <input
+                                type="number"
+                                placeholder="Phone"
+                                required
+                                name="phone"
+                                value={shippingAddress.phone}
+                                onChange={(e) => handleShipping(e)}
+                            />
 
+                            <button type="submit" className="--btn --btn-primary --btn-block">
+                                View order & checkout
+                            </button>
+                        </Card>
+                        <br/>
+                        <br/>
+                    </div>
+                </form>
+            </div>
+        </section>
     )
 }
 
