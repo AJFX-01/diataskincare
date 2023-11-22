5725f54301010fed4427241154b5bae83905cbe3
AJFX-01 <opeyemi.ajegbomogun@yahoo.com>
2023-11-22 16:22:54 +0100

    done styling

--- authcontext.tsx
+++ authcontext.tsx
@@ -1,13 +1,2 @@
-export default function ChatBot() {
-  return (
-    <Card cardClass={styles.card}>
-      <div className={styles["bot-container"]}>
-        <Chatbot
-          config={config}
-          messageParser={MessageParser}
-          actionProvider={ActionProvider}
-        />
-      </div>
-    </Card>
-  );
+
 }
